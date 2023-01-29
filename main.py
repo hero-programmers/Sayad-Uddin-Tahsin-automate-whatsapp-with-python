26e7b5dc01845e513a637043bbf1104321361c8a
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2023-01-29 18:51:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg(phone_no='+8801*********', message="Hey Dear!", time_hour=0, time_min=0)
+phone = input("Enter the Recipient Number (include the country code). Ex. +8801*********\n>>> ")
+message = input("Enter the message\n>>> ")
+time = input("Enter the desired time in 24 Hours. Format: HH:MM\n>>> ")
+hour, minute = time.split(":", 1)
+hour = int(hour) if not str(hour).startswith("0") else int(str(hour).replace("0", "", 1))
+minute = int(minute) if not str(minute).startswith("0") else int(str(minute).replace("0", "", 1))
+
+pywhatkit.sendwhatmsg(phone_no=phone, message=message, time_hour=hour, time_min=minute, tab_close=True)
