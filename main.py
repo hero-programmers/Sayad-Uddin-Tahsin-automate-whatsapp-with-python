4eb38ab9f28be6a72b80c3cf6c3f8a55a96f0549
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2023-01-29 17:42:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg(phone_no='+8801*********', message="Hey Dear!", time_hour=24, time_min=9)
+pywhatkit.sendwhatmsg(phone_no='+8801*********', message="Hey Dear!", time_hour=0, time_min=0)
